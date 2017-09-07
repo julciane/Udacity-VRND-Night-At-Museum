97707e047c64bb8f707b7e78f35a02eca3ab80a9
julciane <julciane@users.noreply.github.com>
2017-09-07 13:41:50 -0300

    Adding sphere to play 360 video

--- Auto_VR/Assets/NavigationScript.cs
+++ Auto_VR/Assets/NavigationScript.cs
@@ -1,13 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NavigationScript : MonoBehaviour {
 
     public GameObject _canvasInfo;
+    public Transform _originalPoint;
+    public Transform _featurePoint;
 
     public void DismissCanvas()
     {
         _canvasInfo.SetActive(false);
+    }
+
+    public void LoadFeature()
+    {
+        Camera.main.transform.position = _featurePoint.position;
     }
 }
