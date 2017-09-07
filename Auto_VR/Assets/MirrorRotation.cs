77bfe337139e0a381d2c67510939629bd8f0772e
julciane <julciane@users.noreply.github.com>
2017-09-07 18:21:24 -0300

    movies inside sphere and cinema

---
+++ Auto_VR/Assets/MirrorRotation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorRotation : MonoBehaviour {
+    public Camera vrCamera;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.rotation = Quaternion.Euler(new Vector3(0f, 180f, 0f)) * vrCamera.transform.rotation;
+    }
+}
---
+++ Auto_VR/Assets/StreamVideo.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+//using UnityEngine.Video;
+
+public class StreamVideo : MonoBehaviour {
+
+    public string url;
+    public GameObject sphere;
+    public VideoClip videoToPlay;
+
+    private VideoPlayer videoPlayer;
+    private VideoSource videoSource;
+    private SphereCollider a;
+
+    private AudioSource audioSource;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Application.runInBackground = true;
+        //StartCoroutine(play360Video());
+    }
+
+    /*override void OnTriggerEnter()
+    {
+
+    }*/
+
+    IEnumerator play360Video()
+    {
+        videoPlayer = gameObject.AddComponent<VideoPlayer>();
+        audioSource = gameObject.AddComponent<AudioSource>();
+
+        videoPlayer.playOnAwake = false;
+        audioSource.playOnAwake = false;
+        audioSource.Pause();
+
+        //videoPlayer.source = VideoSource.Url;
+        //videoPlayer.url = url;
+
+
+        videoPlayer.audioOutputMode = VideoAudioOutputMode.AudioSource;
+
+        videoPlayer.EnableAudioTrack(0, true);
+        videoPlayer.SetTargetAudioSource(0, audioSource);
+
+        //videoPlayer.renderer = sphere.GetComponent<MeshRenderer>();
+
+        videoPlayer.source = VideoSource.VideoClip;
+        videoPlayer.clip = videoToPlay;
+        videoPlayer.Prepare();
+
+        WaitForSeconds waitTime = new WaitForSeconds(1.0f);
+
+        while (!videoPlayer.isPrepared)
+        {
+            yield return waitTime;
+            break;
+        }
+        //sphere.GetComponent<Renderer>().material.SetTexture("_MainTex", videoPlayer.texture);
+
+        videoPlayer.Play();
+        audioSource.Play();
+
+
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
